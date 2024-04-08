8c1ab821edfb1aacbfbac25507e4c4cc4ec6fb08
Ciobanu Bianca <zilesimilare@gmail.com>
2024-04-08 09:54:02 +0300

    set up

--- test1.c
+++ test1.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    printf("hello world!\n") ;
+    char line[1000] ;
+
+    FILE *f1 = fopen("date/t1/d.in", "rt") ;
+    if(f1 == NULL)
+    {
+        puts("Fisierul nu poate fi deschis!\n") ;
+        exit(1) ;
+    }
+
+    while (fgets(line, sizeof(line), f1) != NULL)
+    {
+    
+    }
+ 
+    FILE *f2 = fopen("out/r.out", "wt") ;
+    if(f2 == NULL)
+    {
+        puts("Fisierul nu poate fi deschis!\n") ;
+        exit(1) ;
+    }
+
+    fclose(f1) ;
+    fclose(f2);
+
     return 0 ;
 }
